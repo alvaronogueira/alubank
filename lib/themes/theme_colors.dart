07434c2015563289b9f417bb5c35ac3a3394fce8
Álvaro Nogueira <98788990+alvaronogueira@users.noreply.github.com>
2023-08-12 15:27:04 -0300

    create theme_colors.dart

---
+++ lib/themes/theme_colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ThemeColors {
+  static const MaterialColor primaryColor = Colors.purple;
+  static const List<Color> headerGradient = [
+    Color.fromRGBO(103, 99, 234, 1),
+    Color.fromRGBO(155, 105, 254, 1),
+    Color.fromRGBO(195, 107, 255, 1),
+  ];
+
+  //mapa com nomes da seções
+  static const recentActivity = {
+    'spent': Color.fromRGBO(255, 175, 29, 1),
+    'income': Color.fromRGBO(191, 69, 198, 1),
+  };
+  static const division = Color.fromRGBO(166, 166, 166, 1);
+}
