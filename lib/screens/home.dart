336ed559ade677cebfb34c07ef97c2558f89ac4c
Álvaro Nogueira <98788990+alvaronogueira@users.noreply.github.com>
2023-08-12 15:26:46 -0300

    update home

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,18 +1,21 @@
 import 'package:alubank/components/box_card.dart';
+import 'package:alubank/components/color_dot.dart';
+import 'package:alubank/components/content_division.dart';
 import 'package:alubank/components/sections/header.dart';
+import 'package:alubank/themes/theme_colors.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
-  const Home({super.key});
+  const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
         children: <Widget>[
           Header(),
           BoxCard(
-            boxContent: Text('Hello world!'),
+            boxContent: ContentDivision(),
           ),
         ],
       ),
