9056eaaab37c6f65a49a7d251f1e452f43caf282
Álvaro Nogueira <98788990+alvaronogueira@users.noreply.github.com>
2023-08-14 13:20:10 -0300

    create recent_activity.dart

---
+++ lib/components/sections/recent_activty.dart
@@ -0,0 +1,100 @@
+import 'package:alubank/components/box_card.dart';
+import 'package:alubank/components/color_dot.dart';
+import 'package:alubank/components/content_division.dart';
+import 'package:alubank/themes/theme_colors.dart';
+import 'package:flutter/material.dart';
+
+class RecentActivty extends StatelessWidget {
+  const RecentActivty({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: BoxCard(boxContent: _RecentActivityContent()),
+    );
+  }
+}
+
+class _RecentActivityContent extends StatelessWidget {
+  const _RecentActivityContent({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(right: 4.0),
+                  child: ColorDot(
+                    color: ThemeColors.recentActivity['spent'],
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Cash Outflow'),
+                    Text('\$9900.97',
+                        style: Theme.of(context).textTheme.bodyLarge),
+                  ],
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(right: 4.0),
+                  child: ColorDot(
+                    color: ThemeColors.recentActivity['income'],
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Cash Inflow'),
+                    Text('\$7890.97',
+                        style: Theme.of(context).textTheme.bodyLarge),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 16.0, bottom: 8.0),
+          child: Text('Spent Limits: \$526.64'),
+        ),
+        Container(
+          clipBehavior: Clip.hardEdge,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(5),
+          ),
+          child: LinearProgressIndicator(
+            value: 0.46,
+            minHeight: 8,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+          child: ContentDivision(),
+        ),
+        Text(
+            'You spent \$2419.90 with games this mounth. Try to lower that cost.'),
+        TextButton(
+          onPressed: () {},
+          child: Text(
+            'Tell me how!',
+            style: TextStyle(
+              fontSize: 16,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
