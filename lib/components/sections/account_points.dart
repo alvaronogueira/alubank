319832ca4de69e842a174c5306a7defd17489cb5
Álvaro Nogueira <98788990+alvaronogueira@users.noreply.github.com>
2023-08-14 17:14:46 -0300

    create account_points

---
+++ lib/components/sections/account_points.dart
@@ -0,0 +1,46 @@
+import 'package:alubank/components/box_card.dart';
+import 'package:flutter/material.dart';
+
+class AccounPoints extends StatelessWidget {
+  const AccounPoints({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(bottom: 16.0),
+            child: Text(
+              'Points account',
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+          ),
+          BoxCard(boxContent: ),
+        ],
+      ),
+    );
+  }
+}
+
+
+class _AccountPoints extends StatelessWidget {
+  const _AccountPoints({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(padding: const EdgeInsets.only(bottom: 8.0), child: Text('Total points'),
+      ),
+      ],
+    );
+  }
+}
+
+
+
+
